af65fc9941876ba5e67332a2a240d4a9ba5b3bf3
cbabhulkar <c.babhulkar@gmail.com>
2022-07-12 16:43:55 +0530

    AbstractClass

--- Abstractclass/Abstractclass.cpp
+++ Abstractclass/Abstractclass.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 class Base{
 	public:
-	virtual int sum(int a,int b)=0;  /*pure virtual function*/
-	                       /*A Class in which pure virtual function is
-				  present is called as Abstract Class*/
-};                          /*Abstract class is use for data abstraction or data				 hiding and to provide appropriate base class
-			      	    from which other classes can inherit*/
+	Base(){
+	cout<<"Object Created"<<endl;
+	}
+
+	virtual int sum(int a,int b)=0;  /*pure virtual function*/     		
+		
+/*A Class in which pure virtual function is present is called Abstract class*/
+
+};
 
 class Derived:public Base{
 	
@@ -19,8 +23,8 @@ class Derived:public Base{
 };
 
 int main(){
-//	Base obj;          	/*Abstract class does not create object*/
-	Derived obj1;
-	obj1.sum(3,4);
+//	Base objBase;          	/*Abstract class does not create object*/
+	Derived objDerived;
+	objDerived.sum(3,4);
 	return(0);
 }
